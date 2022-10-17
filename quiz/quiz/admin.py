d1693f5eec2e6a465df55cb1021856c119f2d200
Piyush <piyushb544@gmail.com>
2022-10-17 05:44:46 +0530

    models are prepared

--- quiz/quiz/admin.py
+++ quiz/quiz/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . models import Profile,Assessment,QuestionSet
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Assessment)
+admin.site.register(QuestionSet)
--- quiz/quiz/models.py
+++ quiz/quiz/models.py
@@ -1,3 +1,34 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.urls import reverse
+from datetime import datetime, date
+# from django.db.models import DateTimeField
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(
+        null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+class Assessment(models.Model):
+    user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 255, default="Exam")
+    duration = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return(self.user + '-' +self.name)
+
+
+class QuestionSet(models.Model):
+    assessment = models.OneToOneField(Assessment, null = True, on_delete = models.CASCADE)
+    # TODO add question image feature 
+    questionTitle = models.CharField(max_length = 1023, default= "question statement")
+    information = models.CharField(max_length = 10000)
