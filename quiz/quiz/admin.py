b055a99a86f0c8498e4fa4a652dfe51ffd944cf7
Piyush <piyushb544@gmail.com>
2022-11-17 16:07:05 +0530

    created new model CandidateDetails

--- quiz/quiz/admin.py
+++ quiz/quiz/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from . models import Profile, Assessment, QuestionSet, OptionSet, Evaluation
+from . models import Profile, Assessment, QuestionSet, OptionSet, Evaluation, CandidateDetail
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Assessment)
 admin.site.register(QuestionSet)
 admin.site.register(OptionSet)
 admin.site.register(Evaluation)
+admin.site.register(CandidateDetail)
--- quiz/quiz/models.py
+++ quiz/quiz/models.py
@@ -7,6 +7,8 @@ from datetime import datetime, date
 # from django.db.models import DateTimeField
 
 # Create your models here.
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
@@ -18,31 +20,68 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user)
 
+
 class Assessment(models.Model):
-    user = models.ForeignKey(User, null = False, on_delete = models.CASCADE)
-    name = models.CharField(max_length = 255, default="Exam")
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, default="Exam")
     duration = models.IntegerField()
     time = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return(self.name)
 
 
 class QuestionSet(models.Model):
-    assessment = models.ForeignKey(Assessment, null = False, on_delete = models.CASCADE)
-    # TODO add question image feature 
-    questionTitle = models.CharField(max_length = 1023, default= "question statement")
+    assessment = models.ForeignKey(
+        Assessment, null=False, on_delete=models.CASCADE)
+    # TODO add question image feature
+    questionTitle = models.CharField(
+        max_length=1023, default="question statement")
     mark = models.IntegerField()
+
     def __str__(self):
         return(str(self.assessment))
 
+
 class OptionSet(models.Model):
-    Question = models.ForeignKey(QuestionSet, null = True, on_delete = models.CASCADE)
-    optionStatement = models.CharField(max_length = 1023)
+    Question = models.ForeignKey(
+        QuestionSet, null=True, on_delete=models.CASCADE)
+    optionStatement = models.CharField(max_length=1023)
     correct = models.BooleanField(default=False)
+
     def __str__(self):
         return(self.optionStatement)
-    
+
+
 class Evaluation(models.Model):
-    candidate = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
+    candidate = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     marks = models.IntegerField()
-    
+
+
+class CandidateDetail(models.Model):
+    assessment = models.ForeignKey(
+        Assessment, null=False, on_delete=models.CASCADE)
+    fullName = models.BooleanField(default=False)
+    workExperience = models.BooleanField(default=False)
+    city = models.BooleanField(default=False)
+    rollNo = models.BooleanField(default=False)
+    email = models.BooleanField(default=False)
+    gradYear = models.BooleanField(default=False)
+    cgpa = models.BooleanField(default=False)
+    gpa = models.BooleanField(default=False)
+    collegeName = models.BooleanField(default=False)
+    contactNo = models.BooleanField(default=False)
+    contactRec = models.BooleanField(default=False)
+    stream = models.BooleanField(default=False)
+    branch = models.BooleanField(default=False)
+    major = models.BooleanField(default=False)
+    degree = models.BooleanField(default=False)
+    gender = models.BooleanField(default=False)
+    jobRole = models.BooleanField(default=False)
+    resume = models.BooleanField(default=False)
+    disclaimer = models.CharField(
+        max_length=5000, default="I agree not to copy code from any source(including websites, books or friends and colleagues) to complete this assessment. I may, however, reference programming language documentation or use an IDE that has code completion features")
+    disclaimerCheck = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.assessment
